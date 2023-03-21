c49c46871ceb09284b27398bfa50cb78a1138d4b
dvdeo <127682291+dvdev04@users.noreply.github.com>
2023-03-22 02:01:02 +0900

    Add files via upload

---
+++ Calculator/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    private int menuChoice;
+
+    public void setMenuChoice(int menuChoice) {
+        this.menuChoice = menuChoice;
+    }
+
+    public int getMenuChoice() {
+        return menuChoice;
+    }
+}
